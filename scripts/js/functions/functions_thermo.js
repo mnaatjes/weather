93bbf578d7c7ec2d7c6818f1a4db64bff7868b02
mnaatjes <michael.naatjes87@gmail.com>
2024-09-20 10:31:47 -0400

    commit before branching thermo

--- scripts/js/functions/functions_thermo.js
+++ scripts/js/functions/functions_thermo.js
@@ -2,6 +2,10 @@
 // CIS 174: Functions - Thermo
 //-------------------------------------------------------------------------------------------------------
 const fahrenheit = document.getElementById('fahrenheit');
+console.log(fahrenheit);
+let f_style = getComputedStyle(fahrenheit);
+console.log(f_style.getPropertyValue('--thermo-height'));
+fahrenheit.style.setProperty('--temp-height', '15%');
 /*------------------------------------------------------*/
 /***
  * @name thermoController
